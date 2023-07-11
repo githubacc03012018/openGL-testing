6ce0f1ab3351b70b6ad811afc95864d628b3b293
Martin DAMYANOV <martin.damyanov@amadeus.com>
2023-07-11 23:14:25 +0300

    glEnableVertexAttribArray

--- OpenGL/Application.cpp
+++ OpenGL/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -18,20 +20,40 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+ 
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW did not init!" << "\n";
+        return 1;
+    }
 
-    /* Loop until the user closes the window */
+    std::cout << glGetString(GL_VERSION) << "\n";
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    float bufferData[6] = {
+        -0.5f, -0.5f,
+        0.f,    0.5f,
+        0.5f,   -0.5f
+    };
+
+    glBufferData(buffer, 6 * sizeof(float), bufferData, GL_STATIC_DRAW);
+
+
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
         glBegin(GL_TRIANGLES);
+
         glColor3f(0, 255, 0);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        
+        //glDrawElements(GL_TRIANGLES, 3, );
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+        glEnableVertexAttribArray(0);
 
         glEnd();
-
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
